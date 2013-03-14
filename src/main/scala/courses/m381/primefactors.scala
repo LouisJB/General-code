601878ee54c1ac2b56376cecfd218b164f78cb5f
LouisB <chillipower.uk@gmail.com>
2013-03-14 22:42:43 +0000

    small update to add mobius and mertens functions

--- src/main/scala/courses/m381/primefactors.scala
+++ src/main/scala/courses/m381/primefactors.scala
@@ -17,9 +17,7 @@ object PrimeFactors extends App {
 
   // return a list of prime factors...
   def primeFactors(n : Int) : List[Int] = {
-
     var primeFactors = List[Int]()
-
     var x = n
     var i = 1
     for (i <- 2 to (x / i) if x > 1) {
@@ -28,7 +26,6 @@ object PrimeFactors extends App {
         x = x / i
       }
     }
-
     primeFactors.reverse
   }
 
@@ -42,8 +39,35 @@ object PrimeFactors extends App {
 
   def divisors(n : Int) = divisorLists(primeFactorPowers(n), List()).filter(_.size > 0).map(e => e.reduceLeft(_ * _))
 
+  def squareFree(x : Int) : Boolean = {
+    val factors = primeFactorPowers(x).toSet
+    squareFree(factors)
+  }
+  type ~>[T, R] = PartialFunction[T, R]
+  def squareFree(factors : Set[(Int, Int)]) : Boolean =
+    !factors.exists{ case (_, power) => power >= 2}
+  def mobius : ~>[Int, Int] = { case n if n >= 1 =>
+    val factors = primeFactorPowers(n)
+    val sf = squareFree(factors.toSet)
+    if (sf && factors.map(_._2).sum % 2 == 0)
+      1
+    else if (sf)
+      -1
+    else
+      0
+  }
+
+  def mertens(n : Int) =
+    (1 to n).map(mobius).sum
+
+  val ms = (1 to 1000).map(x => (x, mobius(x)))
+
+  def count(ls : List[Int], n : Int) = ls.reduce((a , b) => if (b == 1) a+1 else a)
+  val vs = ms.map(_._2).toList
+  val ones = count(vs, 1)
+  val negones = count(vs, -1)
+  println("ones = " + ones + ", net ones = " + negones + ", zeroes = " + (1000 - ones - negones))
+  ms.foreach(println)
+
   println(Primes.primeFactors(args(0).toInt))
 }
-
-
-
