d5ec323289bbbb8840958f25037f0f403a596886
LouisB <chillipower.uk@gmail.com>
2014-05-31 22:37:41 +0100

    commit old solution to Euler problem 66

--- src/main/scala/projecteuler/Problem66.scala
+++ src/main/scala/projecteuler/Problem66.scala
@@ -2,11 +2,32 @@ package projecteuler
 
 import math._
 
+// x2 – Dy2 = 1
 object Problem66 {
 
   def isSquare(n : Int) = { val r = sqrt(n); (r * r == n) }
 
   def main(args : Array[String]) {
-
+    var dMax = 0
+    var xMax = 0
+    (2 to 100).foreach { d =>
+      if (!isSquare(d)) {
+        (1 to 100000).find { x =>
+          (1 to 50000).find { y =>
+            val a = x*x - d*y*y
+            //println("a = " + a  + ", d = " + d + ", x = " + x + ", y = " + y)
+            if (a == 1) {
+              if (x > xMax) {
+                xMax = x
+                dMax = d
+                println("dMax = " + dMax + ", at x = " + x + ", y = " + y)
+              }
+              true
+            }
+            else false
+          }.isDefined
+        }
+      }
+    }
   }
 }
